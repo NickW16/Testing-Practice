1e3f00522e234fcd3a57c5f0e7af374f5b766fa8
NickW16 <171855220+NickW16@users.noreply.github.com>
2025-08-26 16:47:45 -0300

    add calculator and caesarCipher functions

--- main.js
+++ main.js
@@ -1,14 +1,58 @@
 function sum(a, b) {
   return a + b;
 }
-
+// turns first letter into capitalized letter
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
+// reverts a string
 function reverseString(string) {
     return string.split('').reverse().join('');
 }
+// simple calculator
+const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    return a / b;
+  }
+}
+
+// Caesar Shift Cipher
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+function caesarCipher(text, shift) {
+  let result = '';
+  for(let i = 0; i < text.length; i++) {
+    const char = text[i];
+
+    if(alphabet.includes(char.toLowerCase())) {
+      const lowerChar = char.toLowerCase();
+      const currentIndex = alphabet.indexOf(lowerChar);
+
+      // handle negative shift by adding 26...
+      const newIndex = (currentIndex + shift + 26) % 26;
+      let newChar = alphabet[newIndex];
+
+      // Preserve original case
+      if (char === char.toUpperCase()) {
+        newChar = newChar.toUpperCase();
+      }
+      // append to result after all conditions pass
+      result += newChar;
+    } else {
+      result += char;
+    }
+  }
+  return result;
+}
 
 // for exporting, it has to be an object
-module.exports = { sum, capitalize, reverseString };
+module.exports = { sum, capitalize, reverseString, calculator, caesarCipher };
--- main.test.js
+++ main.test.js
@@ -2,6 +2,8 @@
 const { sum } = require('./main');
 const { capitalize } = require('./main');
 const { reverseString } = require('./main');
+const { calculator } = require('./main');
+const { caesarCipher } = require('./main');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -15,3 +17,19 @@ test('Function correctly reverses the string provided', () => {
   expect(reverseString('word')).toMatch('drow');
   expect(reverseString('Car')).toMatch('raC');
 });
+
+test('Calculator correctly completes all four operations', () => {
+  expect(calculator.add(2, 2)).toBe(4);
+  expect(calculator.add(-3, 5)).toBe(2);
+  expect(calculator.subtract(320, 200)).toBe(120);
+  expect(calculator.subtract(-200, 100)).toBe(-300);
+  expect(calculator.multiply(3, 8)).toBe(24);
+  expect(calculator.multiply(40, 2)).toBe(80);
+  expect(calculator.divide(30, 2)).toBe(15);
+  expect(calculator.divide(5, 4)).toBe(1.25);
+});
+
+test('Caesar Shift Cipher function works', () => {
+  expect(caesarCipher('xyz', 3)).toMatch('abc');
+  expect(caesarCipher('Hello, World!', 3)).toMatch('Khoor, Zruog!');
+})
